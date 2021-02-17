8ce605b2295aa51c01d90298db5f4c82f5c85669
Emmanuel <d.emmanuel.duque@gmail.com>
2021-02-17 07:55:54 +0100

    code2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedComponentsModule } from './components/shared-components.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, SharedComponentsModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
